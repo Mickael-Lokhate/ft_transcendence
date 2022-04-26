5090743cc610d405250add58cee7bb9b0cea7df7
Mickael Lokhate <mlokhate@student.42nice.fr>
2022-04-26 17:33:25 +0200

    init clear commit

---
+++ infinity/src/environments/common.ts
@@ -0,0 +1,12 @@
+const HOST = 'localhost';
+// test
+//const HOST = '10.12.2.1';
+
+export const commonEnv = {
+  API_INFINITY: 'http://' + HOST + ':3000',
+  API_GATAWAY: 'ws://' + HOST + ':3002',
+  GAME_GATEWAY: 'ws://' + HOST + ':3000',
+  FORTY_TWO_ID:
+  INF_HOST: HOST,
+  INF_PORT: '4200',
+};
